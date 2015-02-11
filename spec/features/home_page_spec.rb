a615271769ce0a986344f5e142118590fb7d75e1
Jake Marwil <rjm02006@gmail.com>
2015-02-11 13:33:47 -0700

    confirm homepage test

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -6,9 +6,10 @@ describe 'User can visit homepage' do
   scenario 'User visits root of application' do
 
     visit '/'
-    
+
     expect(page).to have_content("Locations")
 
   end
 
 end
+#confirm homepage test
