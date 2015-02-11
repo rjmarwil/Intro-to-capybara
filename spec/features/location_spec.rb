9e960a9e111aba1248a95570af8022e5fb072b4e
Jake Marwil <rjm02006@gmail.com>
2015-02-11 13:59:35 -0700

    users can edit a location

--- spec/features/location_spec.rb
+++ spec/features/location_spec.rb
@@ -51,7 +51,33 @@ describe 'User can CRUD locations' do
   end
 
   scenario 'User can edit a location' do
-    #fill in
+    # visit homepage
+    visit '/'
+
+    # click on link to go to new location form
+    click_on "New Location"
+
+    #filling out form for location
+    fill_in 'location[name]', :with => "Jake"
+    fill_in 'location[address]', with: "New Address"
+    fill_in 'location[zipcode]', with: "90210"
+
+    #submitting form to create a location
+    click_on "Create Location"
+
+    # click on link to existing location
+    click_on "Edit"
+
+    # change  zip code field and update it 5000
+
+    fill_in 'location[zipcode]', with: "5000"
+
+    #submitting form to update a zipcode in location
+    click_on "Update Location"
+
+    #expect page to show updated zipcode 5000
+    expect(page).to have_content("5000")
+
   end
 
 
